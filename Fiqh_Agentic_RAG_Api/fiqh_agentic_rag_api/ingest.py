3ca0c5cc8566fd008fe792ad6b17d03d66663410
mehmetpektass <pektasmehmett1184@gmail.com>
2025-12-24 14:06:06 +0300

    complete qdrant vector db with external 4 books

---
+++ Fiqh_Agentic_RAG_Api/fiqh_agentic_rag_api/ingest.py
@@ -0,0 +1,59 @@
+from dotenv import load_dotenv
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, Settings
+from llama_index.vector_stores.qdrant import QdrantVectorStore
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from qdrant_client import QdrantClient
+from llama_index.core.node_parser import SentenceSplitter
+
+load_dotenv()
+
+def ingest_data():
+    print("🔄 Data ingestion process starting...")
+
+    # Initialize Local Hugging Face Model
+    print("📥 Loading local Hugging Face model (paraphrase-multilingual-MiniLM-L12-v2)...")
+    embed_model = HuggingFaceEmbedding(
+        model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+    )
+    
+    Settings.embed_model = embed_model
+    Settings.llm = None
+    
+    # Chunking Settings
+    Settings.text_splitter = SentenceSplitter(chunk_size=512, chunk_overlap=50)
+    
+    # Load Documents
+    data_path = "./src/fiqh_agentic_rag_api/data"
+    print(f"📂 Reading files from: {data_path}")
+    documents = SimpleDirectoryReader(data_path, recursive=True).load_data()
+    
+    if not documents:
+        print("⚠️ ERROR: No files found in the specified directory!")
+        return
+
+    print(f"✅ Total {len(documents)} document chunks found.")
+
+    # Connect to Qdrant
+    client = QdrantClient(url="http://localhost:6333")
+    
+    # because vector dimensions are different now.
+    vector_store = QdrantVectorStore(
+        client=client,
+        collection_name="fiqh_knowladge_base"
+    )
+    
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    
+    print("🚀 Embedding and Indexing started (Running locally)...")
+    
+    VectorStoreIndex.from_documents(
+        documents=documents,
+        storage_context=storage_context,
+        show_progress=True
+    )
+
+    print("\n🎉 Success! All data ingested into Qdrant using local embeddings.")
+    print("You can the chunks on http:localhost:6333/dashboard.")
+    
+if __name__ == "__main__":
+    ingest_data()
